4f2788cf6409f3ebfa71dda8c54137bf673f9e32
skcn022 <skcn022@gmail.com>
2017-05-20 15:04:54 +0700

    end date 20-may-60

--- app/src/main/java/net/flowdee/mytraffic/MyData.java
+++ app/src/main/java/net/flowdee/mytraffic/MyData.java
@@ -6,4 +6,22 @@ package net.flowdee.mytraffic;
 
 public class MyData {
 
+    // Explicit
+    private String[] titleStrings = new String[]{"หัวข้ออที่ 1", "หัวข้ออที่ 2", "หัวข้ออที่ 3", "หัวข้ออที่ 4", "หัวข้ออที่ 5",
+            "หัวข้ออที่ 6", "หัวข้ออที่ 7", "หัวข้ออที่ 8", "หัวข้ออที่ 9", "หัวข้ออที่ 10", "หัวข้ออที่ 11", "หัวข้ออที่ 12", "หัวข้ออที่ 13",
+            "หัวข้ออที่ 14", "หัวข้ออที่ 15", "หัวข้ออที่ 16", "หัวข้ออที่ 17", "หัวข้ออที่ 18", "หัวข้ออที่ 19", "หัวข้ออที่ 20" };
+    private String[] detailString; //= new String[20];
+
+    public String[] getTitleStrings() {
+        return titleStrings;
+    }
+
+    public String[] getDetailString() {
+
+        String[] resultStrings
+
+
+
+        return detailString;
+    }
 }  // main card
